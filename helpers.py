83e419a46ca6bb7c5b7201b39b210cfd2f8defc2
Nearlybird <49175582+Nearlybird@users.noreply.github.com>
2019-05-20 14:07:06 -0600

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,10 +1,19 @@
-
-
 def alphabet_position(letter):
-    #Add your code here
-    return
-
+    letter = letter.lower()
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    return alpha.find(letter)
 
 def rotate_character(char, rot):
-    #Add your code here
-    return
+    output = ""
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    num = alphabet_position(char)
+    num += rot
+    if num % 26 >= 0:
+        num = num % 26
+        output = alpha[num]
+    else:
+        output = alpha[num]
+    if char.isupper():
+        return output.upper()
+    else:
+        return output.lower()
